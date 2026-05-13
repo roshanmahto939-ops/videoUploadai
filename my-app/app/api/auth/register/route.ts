e9666bdb964f394338b0c6ea32ed85cde5bb0c82
roshan mahto <roshanmahto939@gmail.com>
2026-05-13 11:53:08 +0530

    authentication register complete

---
+++ my-app/app/api/auth/register/route.ts
@@ -0,0 +1,41 @@
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and password are required" },
+        { status: 400 },
+      );
+    }
+    await connectToDatabase();
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "User already registered" },
+        { status: 400 },
+      );
+    }
+    await User.create({
+        email,
+        password
+    })
+
+    return NextResponse.json(
+        {message:"User registration successfully"},
+        {status:400}
+    );
+  } catch (error) {
+    console.error("Registration error",error);
+    
+      return NextResponse.json(
+        {error:"failed to register user"},
+        {status:400}
+    );
+  }
+}
--- my-app/lib/db.ts
+++ my-app/lib/db.ts
@@ -1,34 +1,32 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const  MONGODB_URI=process.env.MONGODB_URI!
+const MONGODB_URI = process.env.MONGODB_URI!;
 
-if(!MONGODB_URI){
-    throw new Error ("Please define mongo_uri in env variable");
+if (!MONGODB_URI) {
+  throw new Error("Please define mongo_uri in env variable");
 }
 
- let cached=global.mongoose
+let cached = global.mongoose;
 
- if(!cached){
-    cached=global.mongoose={conn:null,promise:null}
- }
- export async function connectToDatabase(){
-    if(cached.conn){
-        return cached.conn
-    }
-    if(!cached.promise){
-        const opts={
-            bufferCommands:true,
-            maxPoolSize:10
-        }
-        mongoose
-        .connect(MONGODB_URI)
-        .then(()=>mongoose.connection)
-    }
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+    mongoose.connect(MONGODB_URI).then(() => mongoose.connection);
+  }
 
-    try{
-        cached.conn=await cached.promise
-    }catch(error){
-cached.promise=null
-throw error
-    }
- }
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+}
